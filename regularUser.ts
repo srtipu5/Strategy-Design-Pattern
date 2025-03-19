cb3071a8b7cd2c28cda72b3668a31cf1039bec91
sahiqur.tipu <sahiqur.tipu@technonext.com>
2025-03-19 15:42:35 +0600

    2nd commit

--- regularUser.ts
+++ regularUser.ts
@@ -1,6 +1,6 @@
 import { IDiscountStrategy } from "./discountStrategy";
 export class RegularUser implements IDiscountStrategy {
     calculateAmount(amount: number): number {
-      return amount * 0.9; // 10% off
+      return amount;
     }
   }
